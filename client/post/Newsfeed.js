3e3a71e0e301cf80f6fc6d789e2229fad2da3a52
EKaxada <enocksdha@gmail.com>
2020-12-22 21:25:50 +0300

    added post feature and its actions

--- client/post/Newsfeed.js
+++ client/post/Newsfeed.js
@@ -1,10 +1,32 @@
 import { Card, Divider, Typography } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewPost from "./NewPost.js";
 import PostList from "./PostList.js";
+import { listNewsFeed } from "./api-post.js";
 
 export default function Newsfeed() {
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const Signal = abortController.signal;
+
+    listNewsFeed({
+      userId: jwt.user._id
+      }, {
+      t: jwt.token
+      }, signal).then((data) => {
+      if (data.error) {
+      console.log(data.error)
+      } else {
+      setPosts(data)
+      }
+      })
+
+    return () => {
+      abortController.abort()
+    };
+  }, []);
 
   const addPost = (post) => {
     const updatedPosts = [...posts];
---
+++ client/post/api-post.js
@@ -0,0 +1,87 @@
+const listNewsFeed = async (params, credentials, signal) => {
+  try {
+    let response = await fetch("/api/posts/feed/" + params.userId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const listByUser = async (req, res) => {
+  try {
+    let posts = await Post.find({ postedBy: req.profile._id })
+      .populate("comments.postedBy", "_id name")
+      .populate("postedBy", "_id name")
+      .sort("-created")
+      .exec();
+    res.json(posts);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+
+const create = async (params, credentials, post) => {
+  try {
+    let response = await fetch("/api/posts/new/" + params.userId, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: post,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const like = async (params, credentials, postId) => {
+  try {
+    let response = await fetch("/api/posts/like/", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify({ userId: params.userId, postId: postId }),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const comment = async (params, credentials, postId, comment) => {
+  try {
+    let response = await fetch("/api/posts/comment/", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify({
+        userId: params.userId,
+        postId: postId,
+        comment: comment,
+      }),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default { listNewsFeed, listByUser, create, like };
