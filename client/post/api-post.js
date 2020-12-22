0f41c8642ff1b88c42d1d42b44b77f9196447288
EKaxada <enocksdha@gmail.com>
2020-12-22 21:37:59 +0300

    fixed bugs by adding missing objects

--- client/post/api-post.js
+++ client/post/api-post.js
@@ -84,4 +84,67 @@ const comment = async (params, credentials, postId, comment) => {
   }
 };
 
-export default { listNewsFeed, listByUser, create, like };
+const uncomment = async (params, credentials, postId, comment) => {
+  try {
+    let response = await fetch("/api/posts/uncomment/", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify({
+        userId: params.userId,
+        postId: postId,
+        comment: comment,
+      }),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const unlike = async (params, credentials, postId) => {
+  try {
+    let response = await fetch("/api/posts/unlike/", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify({ userId: params.userId, postId: postId }),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch("/api/posts/" + params.postId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export {
+  listNewsFeed,
+  listByUser,
+  create,
+  like,
+  comment,
+  uncomment,
+  unlike,
+  remove,
+};
