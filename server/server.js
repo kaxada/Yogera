4ece59272f95d82fce13e501bde784cbc355fd8c
Enock Kasaadha <enocksdha@gmail.com>
2021-05-12 17:51:40 +0300

    changed configs

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ import app from './express'
 import mongoose from 'mongoose'
 
 // Connection URL
+const mongoUri = "mongodb+srv://EKaxada:yogera@cluster0.rl9xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+    throw new Error(`unable to connect to database: ${mongoUri}`)
 })
 
 app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
 })
