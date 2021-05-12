387674c48677a414f205c4da90ecab87dfadbf30
Enock Kasaadha <enocksdha@gmail.com>
2021-05-12 17:36:13 +0300

    configured cloud database

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/yogera'
+    mongoUri: process.env.MONGODB_URI
 }
 
 export default config
